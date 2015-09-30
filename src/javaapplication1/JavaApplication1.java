9441bd3c4d7784ce6ff826842de9155cc4ec49a4
usuario <usuario@10.22.143.35>
2015-09-30 10:22:58 +0100

    KATA 2
    segunda version

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,10 +5,11 @@ import java.util.HashMap;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
-        int[] vector = {1,4,7,1,1,8,4,1,4,9,100,-4};
-        Histogram histo = new Histogram(vector);
-        
-        HashMap<Integer, Integer> histogram = histo.getHistograma(vector);
-        System.out.println(histogram);
+        Integer[] vector = {1,4,7,1,1,8,4,1,4,9,100,-4};
+        String[] vector1 = {"Ana", "Juan", "Pedro", "Ana", "Pedro"};
+        Histogram <String> histogram = CalculaHistograma.calcHisto(vector1);
+        for (Object K : histogram.keySet()) {
+            System.out.println(K + "->" + histogram.get(K));
+        }
     }
 }
